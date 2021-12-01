562e1bdacf6f201cc06664fab99d1d1c7c0c7a5d
Arnas <72468560+arnasofc@users.noreply.github.com>
2021-12-01 15:42:56 +0200

    v0.1 - Veikiantis vidurkis

--- main.cpp
+++ main.cpp
@@ -3,68 +3,99 @@
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <vector>
+#include <stdlib.h>
+#include<numeric>
 using namespace std;
 
 class dienynas {
 public:
-    string vardas[100];
-    string pavarde[100];
-    string vidurkis[100];
-    string egzaminas[100];
 
     int studentai;
-    double galutinisV;
+    int namuDarbai;
 
-    char pasirinkimas;
+    vector< string > vardasV;
+    vector< string > pavardeV;
+
+    vector< double > namaiV;
+    vector< double > average;
+    vector < double > egzaminasV;
+    vector < double > galutinisV;
+
+    string vardas;
+    string pavarde;
+    double egzaminas;
+
+    double vidurkisG;
+
+    int suma;
+    int temp;
 
     void input();
-    double finalGrade(double, double);
+    double finalGradeV(double, double);
     void print();
 };
 
 void dienynas::input() {
 
-    cout << "Iveskite kiek studentu pildysite: ";
+    cout << "Kiek studentu pildysite? " << endl;
     cin >> studentai;
-
-    cin.ignore();
 
     for (int i = 0; i < studentai; i++)
     {
         cout << "Iveskite varda: ";
-        getline(cin, vardas[i]);
+        cin >> vardas;
+        vardasV.push_back(vardas);
 
         cout << "Iveskite pavarde: ";
-        getline(cin, pavarde[i]);
+        cin >> pavarde;
+        pavardeV.push_back(pavarde);
 
-        cout << "Iveskite vidurkio bala: ";
-        getline(cin, vidurkis[i]);
+        cout << "Kiek namu darbu padare? ";
+        cin >> namuDarbai;
+
+        for (int k = 1; k <= namuDarbai; k++)
+        {
+            cout << k << " Namu darbo pazymys: ";
+            cin >> suma;
+            namaiV.push_back(suma);
+            temp = accumulate(namaiV.begin(), namaiV.end(), 0) / namuDarbai;
+        }
+
+        average.push_back(temp);
+        namaiV.clear();
 
         cout << "Iveskite egzamino bala: ";
-        getline(cin, egzaminas[i]);
-    }
+        cin >> egzaminas;
+        egzaminasV.push_back(egzaminas);
 
+        finalGradeV(average[i], egzaminasV[i]);
+    }
 }
 
-double dienynas::finalGrade(double vidurkis, double egzaminas) {
-    galutinisV = 0.4 * vidurkis + 0.6 * egzaminas;
-    return galutinisV;
+
+double dienynas::finalGradeV(double vidurkis, double egzaminas) {
+    vidurkisG = 0.4 * vidurkis + 0.6 * egzaminas;
+    galutinisV.push_back(vidurkisG);
 }
 
 void dienynas::print() {
     const char separator = ' ';
     const int nameWidth = 18;
 
+    cout << endl;
     cout << "Pavarde           " << "Vardas            " << "Galutinis (Vid.)" << endl;
     cout << "---------------------------------------------------" << endl;
 
     for (int i = 0; i < studentai; i++)
     {
-        cout << left << setw(nameWidth) << setfill(separator) << pavarde[i];
-        cout << left << setw(nameWidth) << setfill(separator) << vardas[i];
-        cout << left << setw(nameWidth) << setfill(separator) << setprecision(2) << fixed << finalGrade(stoi(vidurkis[i]), stoi(egzaminas[i])) << endl;
+        cout << left << setw(nameWidth) << setfill(separator) << pavardeV[i];
+        cout << left << setw(nameWidth) << setfill(separator) << vardasV[i];
+        cout << left << setw(nameWidth) << setfill(separator) << setprecision(2) << fixed << galutinisV[i];
+        cout << endl;
     }
 
+    cout << endl;
     cout << endl;
 }
 
@@ -72,6 +103,6 @@ int main() {
     dienynas d1;
 
     d1.input();
+    system("CLS");
     d1.print();
-
 }
